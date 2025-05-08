c0e456ae5ca0e2cf781fa07e06ff49f006a21382
Huxvix <umutergut@hotmail.com>
2025-05-08 03:46:19 +0300

    cors allow all

--- tahmin_backend/tahmin_backend/settings.py
+++ tahmin_backend/tahmin_backend/settings.py
@@ -30,10 +30,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',') if os.getenv('ALLOWED_HOSTS') else []
 
-CORS_ALLOWED_ORIGINS = [
-    os.getenv('REACT_APP_URL'),
-]
-
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
