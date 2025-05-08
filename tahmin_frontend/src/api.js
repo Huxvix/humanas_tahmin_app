f5b88ddb331fbdc5b68615b7aa0bc781f59f3fb5
Huxvix <umutergut@hotmail.com>
2025-05-08 12:55:43 +0300

    tweaking the frontend

--- tahmin_frontend/src/api.js
+++ tahmin_frontend/src/api.js
@@ -3,7 +3,7 @@ import.meta.env.VITE_API_BASE_URL;
 
 const AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
-    timeout: 52000, // 52 saniye sonra zaman aşımına uğrayacak (bunu bu kadar uzun tutmamın sebebi render'in bazen çok uzun bekletmesi)
+    timeout: 100000, // 100 saniye sonra zaman aşımına uğrayacak (bunu bu kadar uzun tutmamın sebebi render'in bazen çok uzun bekletmesi)
     headers: {
         'Content-Type': 'application/json',
         accept: 'application/json',
