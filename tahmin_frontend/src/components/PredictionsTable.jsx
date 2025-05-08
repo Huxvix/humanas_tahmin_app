06ccdc320c736d096eb39ebf63d9778a4b32ac26
Huxvix <umutergut@hotmail.com>
2025-05-08 14:05:39 +0300

    placeholder css yerine yazıldı

--- tahmin_frontend/src/components/PredictionsTable.jsx
+++ tahmin_frontend/src/components/PredictionsTable.jsx
@@ -5,7 +5,7 @@ import './PredictionsTable.css';
 
 function PredictionsTable({ data }) {
   return (
-    <div className="overflow-x-auto mx-auto">
+    <div className="overflow-x-auto mx-auto w-full">
       <table className="table table-bordered table-sm table-striped table-hover table-fixed">
         <TableHeader />
         <TableBody data={data} />
